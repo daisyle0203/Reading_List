362469b75bc6a143ce8557daf9111905086ca5b0
Daisy Le <daisy.le0203@gmail.com>
2022-11-29 00:11:05 -0600

    added BookCreate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useState } from "react"
+import BookCreate from "./components/BookCreate"
+
 function App() {
+  const [books, setBooks] = useState([])
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title,
+      },
+    ]
+
+    setBooks(updatedBooks)
+  }
   return (
-    <div>App</div>
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
   )
 }
 
