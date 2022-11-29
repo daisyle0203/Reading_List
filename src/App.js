6a7f2d5954166562d2311d78e08389202a385928
Daisy Le <daisy.le0203@gmail.com>
2022-11-29 00:25:28 -0600

    added BookList and BookShow components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import BookCreate from "./components/BookCreate"
+import BookList from "./components/BookList"
 
 function App() {
   const [books, setBooks] = useState([])
@@ -17,6 +18,7 @@ function App() {
   }
   return (
     <div>
+      <BookList books={books}/>
       <BookCreate onCreate={createBook} />
     </div>
   )
