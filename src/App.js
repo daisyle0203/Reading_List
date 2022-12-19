54a4a3950dba0a37953fab7fa072dadcbc0282e9
Daisy Le <daisy.le0203@gmail.com>
2022-12-18 23:54:55 -0600

    update and delete a record using axios

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
-import { useState } from "react"
+import axios from "axios"
+import { useState, useEffect } from "react"
 import BookCreate from "./components/BookCreate"
 import BookList from "./components/BookList"
 
 function App() {
   const [books, setBooks] = useState([])
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ]
+  // Function to get all the books so they will be displayed when the page first loads
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books")
+
+    setBooks(response.data)
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+  const editBookById = async (id, newTitle) => {
+    const response  = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    })
+
+    console.log(response);
+
+    const updatedBooks = books.map((book) => {
+      if(book.id === id) {
+        return { ...book, ...response.data }
+      }
+
+      return book
+    })
 
     setBooks(updatedBooks)
   }
 
-  const deleteBook = (id) => {
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    })
+
+    console.log(response)
+
+    const updatedBooks = [...books, response.data]
+
+    setBooks(updatedBooks)
+  }
+
+  const deleteBook = async (id) => {
+    axios.delete(`http://localhost:3001/books/${id}`)
+
     const updatedBooks = books.filter((book) => {
-        return book.id !== id
+      return book.id !== id
     })
 
     setBooks(updatedBooks)
   }
   return (
     <div>
-      <BookList books={books} onDelete={deleteBook}/>
+      <BookList onEdit={editBookById}  books={books} onDelete={deleteBook} />
       <BookCreate onCreate={createBook} />
     </div>
   )
