c6b84db7cd9765a843bb0aedee8d695608a8c16d
Daisy Le <daisy.le0203@gmail.com>
2022-12-20 21:08:45 -0600

    refactor the app using useContext hook

--- src/App.js
+++ src/App.js
@@ -1,65 +1,20 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
+import useBooksContext from "./hooks/use-books-context"
 import BookCreate from "./components/BookCreate"
 import BookList from "./components/BookList"
 
 function App() {
-  const [books, setBooks] = useState([])
-
-  // Function to get all the books so they will be displayed when the page first loads
-  const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books")
-
-    setBooks(response.data)
-  }
+  const { fetchBooks } = useBooksContext()
 
   useEffect(() => {
     fetchBooks()
   }, [])
 
-  const editBookById = async (id, newTitle) => {
-    const response  = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle
-    })
-
-    console.log(response);
-
-    const updatedBooks = books.map((book) => {
-      if(book.id === id) {
-        return { ...book, ...response.data }
-      }
-
-      return book
-    })
-
-    setBooks(updatedBooks)
-  }
-
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    })
-
-    console.log(response)
-
-    const updatedBooks = [...books, response.data]
-
-    setBooks(updatedBooks)
-  }
-
-  const deleteBook = async (id) => {
-    axios.delete(`http://localhost:3001/books/${id}`)
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id
-    })
-
-    setBooks(updatedBooks)
-  }
   return (
-    <div>
-      <BookList onEdit={editBookById}  books={books} onDelete={deleteBook} />
-      <BookCreate onCreate={createBook} />
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
     </div>
   )
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,19 @@
+import useBooksContext from "../hooks/use-books-context"
 import BookShow from "./BookShow"
 
-function BookList({ books, onDelete, onEdit }) {
+function BookList() {
+  const { books } = useBooksContext()
+
   const renderedBooks = books.map((book) => {
     return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+      <BookShow key={book.id} book={book} />
     )
   })
-  return <div className="book-list">{renderedBooks}</div>
+  return (
+    <div className="book-list">
+      {renderedBooks}
+    </div>
+  )
 }
 
 export default BookList
